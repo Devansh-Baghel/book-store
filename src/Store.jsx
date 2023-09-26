aa36f1798bee39ffdad349e1086eda70cc5a1c1c
Devansh_Baghel <devanshbaghel85@gmail.com>
2023-09-27 03:13:18 +0530

    added logic to get books from api

--- src/Store.jsx
+++ src/Store.jsx
@@ -1,9 +1,38 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
+
 function Store() {
-  return (
-    <div>
-      <h1>This is the store, List of books to buy lie here</h1>
-    </div>
-  )
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  function getBooks() {
+    axios.get("https://gutendex.com/books/").then((res) => {
+      console.log(res.data.results);
+      console.log(res.data.results[1].formats["image/jpeg"]);
+      setBooks(res.data.results);
+      setLoading(false);
+    });
+  }
+
+  useEffect(() => {
+    getBooks();
+  }, []);
+
+  if (!loading) {
+    return (
+      <div>
+        <h1>This is the store, List of books to buy lie here</h1>
+        <img src={books[1].formats["image/jpeg"]} />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>Please wait books are loading ...</h1>
+      </div>
+    );
+  }
 }
 
-export default Store
+export default Store;
