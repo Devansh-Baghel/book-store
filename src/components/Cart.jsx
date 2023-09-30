00990f4556bd84415c58ba66dc6c48fbd17ab0ef
Devansh_Baghel <devanshbaghel85@gmail.com>
2023-09-30 13:13:07 +0530

    added spinner in cart and wishlist

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,17 +3,20 @@ import { AppContext } from "../App";
 import axios from "axios";
 import { useState } from "react";
 import { useEffect } from "react";
+import Loader from "./Loader";
 
 function Cart() {
   const { cart } = useContext(AppContext);
   const [bookData, setBookData] = useState([]);
   const [hasData, setHasData] = useState();
+  const [loading, setLoading] = useState(true);
 
   function getCartBooks(books) {
     const bookIds = books.join(",");
     axios.get(`https://gutendex.com/books?ids=${bookIds}`).then((res) => {
       setBookData(res.data.results);
       console.log(res.data.results);
+      setLoading(false);
     });
   }
 
@@ -26,13 +29,13 @@ function Cart() {
     }
   }, []);
 
-  if (hasData) {
+  if (hasData && !loading) {
     return (
       <div>
         {bookData.map((book) => (
           <div key={book.id}>
             <h3>{book.title}</h3>
-            <p>{book.authors[0].name}</p>
+            {/* <p>{book.authors[0].name}</p> */}
             <p>{book.id}</p>
             <img src={book.formats["image/jpeg"]} />
             {/* <p>{price}</p> */}
@@ -47,12 +50,14 @@ function Cart() {
         <button>Checkout</button>
       </div>
     );
-  } else {
+  } else if (!hasData) {
     return (
       <div>
         <h1>THE CART IS EMPTY</h1>
       </div>
     );
+  } else if (hasData && loading) {
+    return <Loader />;
   }
 }
 
--- src/components/WishList.jsx
+++ src/components/WishList.jsx
@@ -1,19 +1,20 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { AppContext } from "../App";
 import axios from "axios";
-import { useState } from "react";
-import { useEffect } from "react";
+import Loader from "./Loader";
 
 function WishList() {
   const { wishList } = useContext(AppContext);
   const [bookData, setBookData] = useState([]);
   const [hasData, setHasData] = useState();
+  const [loading, setLoading] = useState(true);
 
   function getCartBooks(books) {
     const bookIds = books.join(",");
     axios.get(`https://gutendex.com/books?ids=${bookIds}`).then((res) => {
       setBookData(res.data.results);
       console.log(res.data.results);
+      setLoading(false);
     });
   }
 
@@ -26,27 +27,29 @@ function WishList() {
     }
   }, []);
 
-  if (hasData) {
-  return (
-    <div>
-      {bookData.map((book) => (
-        <div key={book.id}>
-          <h3>{book.title}</h3>
-          <p>{book.authors[0].name}</p>
-          <p>{book.id}</p>
-          <img src={book.formats["image/jpeg"]} />
-          {/* <p>{price}</p> */}
-        </div>
-      ))}
-    </div>
-  );
-  } else {
+  if (hasData && !loading) {
+    return (
+      <div>
+        {bookData.map((book) => (
+          <div key={book.id}>
+            <h3>{book.title}</h3>
+            {/* <p>{book.authors[0].name}</p> */}
+            <p>{book.id}</p>
+            <img src={book.formats["image/jpeg"]} />
+            {/* <p>{price}</p> */}
+          </div>
+        ))}
+      </div>
+    );
+  } else if (!hasData) {
     return (
       <div>
         <h1>Your wish list is empty</h1>
       </div>
-    )
+    );
+  } else if (hasData && loading) {
+    return <Loader />;
   }
 }
 
-export default WishList
+export default WishList;
