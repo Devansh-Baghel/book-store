1eefdc6e0a23ece1d344fc06b6cb2936e73ab8b2
Devansh_Baghel <devanshbaghel85@gmail.com>
2023-10-01 16:42:43 +0530

    refactored Cart.jsx using swr

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,60 +1,60 @@
-import axios from "axios";
 import { useContext, useState, useEffect } from "react";
 import { AppContext } from "../App";
 import Loader from "./Loader";
 import emptyCart from "../assets/undraw_empty_cart.svg";
 import EmptyList from "./EmptyList";
+import useSWR from "swr";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 function Cart() {
   const { cart } = useContext(AppContext);
-  const [bookData, setBookData] = useState([]);
-  const [hasData, setHasData] = useState();
-  const [loading, setLoading] = useState(true);
-
-  function getCartBooks(books) {
-    const bookIds = books.join(",");
-    axios.get(`https://gutendex.com/books?ids=${bookIds}`).then((res) => {
-      setBookData(res.data.results);
-      console.log(res.data.results);
-      setLoading(false);
-    });
-  }
+  const [isEmpty, setIsEmpty] = useState();
+  const { data, error, isLoading } = useSWR(
+    `https://gutendex.com/books?ids=${cart.join(",")}`,
+    fetcher
+  );
 
   useEffect(() => {
-    getCartBooks(cart);
-    if (cart.length !== 0) {
-      setHasData(true);
+    if (cart.length === 0) {
+      setIsEmpty(true);
     } else {
-      setHasData(false);
+      setIsEmpty(false)
     }
   }, []);
 
-  if (hasData && !loading) {
-    return (
-      <div>
-        {bookData.map((book) => (
-          <div key={book.id}>
-            <h3>{book.title}</h3>
-            {/* <p>{book.authors[0].name}</p> */}
-            <p>{book.id}</p>
-            <img src={book.formats["image/jpeg"]} />
-            {/* <p>{price}</p> */}
-            <div>
-              <button>+</button>
-              <span>1</span>
-              <button>-</button>
-            </div>
-          </div>
-        ))}
+  if (isEmpty) {
+    return <EmptyList src={emptyCart} page={"cart"} />;
+  }
 
-        <button>Checkout</button>
-      </div>
-    );
-  } else if (!hasData) {
-    return <EmptyList src={emptyCart} page={"cart"}/>;
-  } else if (hasData && loading) {
+  if (error) {
+    return <div className="text-2xl text-center mt-20">There was an error</div>;
+  }
+
+  if (isLoading) {
     return <Loader />;
   }
+
+  return (
+    <div>
+      {data.results.map((book) => (
+        <div key={book.id}>
+          <h3>{book.title}</h3>
+          {/* <p>{book.authors[0].name}</p> */}
+          <p>{book.id}</p>
+          <img src={book.formats["image/jpeg"]} />
+          {/* <p>{price}</p> */}
+          <div>
+            <button>+</button>
+            <span>1</span>
+            <button>-</button>
+          </div>
+        </div>
+      ))}
+
+      <button>Checkout</button>
+    </div>
+  );
 }
 
 export default Cart;
