a0c7d5fb70adda388b23131efa33fd29f66f56e9
Devansh_Baghel <devanshbaghel85@gmail.com>
2023-10-02 08:50:29 +0530

    styled store page for tablet and desktop view

--- src/components/Store.jsx
+++ src/components/Store.jsx
@@ -39,30 +39,32 @@ function Store() {
   }
 
   return (
-    <div>
-      <div className="grid grid-col-1 md:grid-cols-2 xl:grid-cols-3 justify-center mt-20">
+    <div className="flex justify-center">
+      <div className="grid grid-col-1 md:grid-cols-2 xl:grid-cols-3 justify-center mt-20 gap-8">
         {data.results.reverse().map((book) => (
           <div
             key={book.id}
-            className="m-8 w-[80vw] sm:w-[60vw] md:w-[40vw] bg-gray-50 shadow-xl rounded-xl border-yellow border-4 flex flex-col p-6 gap-4"
+            className=" w-[75vw] sm:w-[60vw] md:w-[40vw] xl:w-[30vw] bg-gray-50 shadow-xl rounded-xl border-yellow border-4 flex flex-col p-6 gap-4 justify-between md:grid md:grid-cols-2 md:grid-rows-2"
           >
-            <h3 className="text-xl">{book.title}</h3>
-            <img src={book.formats["image/jpeg"]} alt={book.title} />
-            <button
-              className="bg-yellow rounded-md p-2"
-              onClick={() => {
-                addToCart(book.id);
-              }}
-            >
-              Add to Cart
-            </button>
-            <button
-              onClick={() => {
-                addToWishList(book.id);
-              }}
-            >
-              Add to Wish List
-            </button>
+            <h3 className="text-xl md:col-start-2">{book.title}</h3>
+            <img src={book.formats["image/jpeg"]} alt={book.title} className="md:col-start-1 md:row-start-1 md:row-end-3" />
+            <div className="flex flex-col gap-4 md:self-center">
+              <button
+                className="bg-yellow rounded-md p-2"
+                onClick={() => {
+                  addToCart(book.id);
+                }}
+              >
+                Add to Cart
+              </button>
+              <button
+                onClick={() => {
+                  addToWishList(book.id);
+                }}
+              >
+                Add to Wish List
+              </button>
+            </div>
           </div>
         ))}
       </div>
