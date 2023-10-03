ab2be7c26bcfbed709bf8d14e9bbb97b5fd74683
Devansh_Baghel <devanshbaghel85@gmail.com>
2023-10-03 21:59:40 +0530

    add checkout ui

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -30,27 +30,63 @@ function Cart() {
 
   return (
     <div>
-      {data.results.map((book) => {
-        const bookPriceIndex = cart.ids.indexOf(book.id);
-        const bookPrice = cart.prices[bookPriceIndex]
-        return (
-          <div key={book.id}>
-            <h3>{book.title}</h3>
-            <h3>${bookPrice}</h3>
-            {/* <p>{book.authors[0].name}</p> */}
-            <p>{book.id}</p>
-            <img src={book.formats["image/jpeg"]} />
-            {/* <p>{price}</p> */}
-            <div>
-              <button>+</button>
-              <span>1</span>
-              <button>-</button>
+      <div>
+        <h3>Checkout</h3>
+        <hr />
+        <div>
+          <h3>Book Name</h3>
+          <h3>Price</h3>
+        </div>
+        <hr />
+        {data.results.map((book) => {
+          const bookPriceIndex = cart.ids.indexOf(book.id);
+          const bookPrice = cart.prices[bookPriceIndex];
+          return (
+            <div key={book.id}>
+              <span>
+                {book.title.length > 20
+                  ? book.title.slice(0, 20) + "..."
+                  : book.title}
+              </span>
+              <span>${bookPrice}</span>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+        <h3>
+          Grand Total: $
+          {cart.prices.reduce((acc, curr) => {
+            return parseFloat(acc) + parseFloat(curr);
+          }, 0)}
+        </h3>
+        <button>Checkout</button>
+      </div>
+      <div>
+        {data.results.map((book) => {
+          const bookPriceIndex = cart.ids.indexOf(book.id);
+          const bookPrice = cart.prices[bookPriceIndex];
+          return (
+            <div key={book.id}>
+              <h3>
+                {book.title.length > 58
+                  ? book.title.slice(0, 58) + "..."
+                  : book.title}
+              </h3>
+              <h3>${bookPrice}</h3>
+              {/* <p>{book.authors[0].name}</p> */}
+              <p>{book.id}</p>
+              <img src={book.formats["image/jpeg"]} />
+              {/* <p>{price}</p> */}
+              <div>
+                <button>+</button>
+                <span>1</span>
+                <button>-</button>
+              </div>
+            </div>
+          );
+        })}
 
-      <button>Checkout</button>
+        <button>Checkout</button>
+      </div>
     </div>
   );
 }
