fb741ea3aa718181b541337304f5c04a9164eb14
Devansh_Baghel <devanshbaghel85@gmail.com>
2023-10-22 19:29:41 +0530

    fix: changing number of books to order now changes the shown pricing accordingly

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,7 +4,7 @@ import Loader from "./Loader";
 import emptyCart from "../assets/undraw_empty_cart.svg";
 import EmptyList from "./EmptyList";
 import useSWR from "swr";
-import BookCounter from "./BookCounter";
+import CheckoutBooks from "./CheckoutBooks";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
@@ -76,28 +76,7 @@ function Cart() {
         {data.results.map((book) => {
           const bookPriceIndex = cart.ids.indexOf(book.id);
           const bookPrice = cart.prices[bookPriceIndex];
-          return (
-            <div
-              key={book.id}
-              className=" w-[75vw] sm:w-[60vw] md:w-[40vw] xl:w-[30vw] bg-gray-50 shadow-xl rounded-xl border-main_yellow border-4 flex flex-col p-6 gap-4 justify-between md:grid md:grid-cols-2"
-            >
-              <div className="flex flex-col gap-1 md:gap-4">
-                <h3 className="text-xl">
-                  {book.title.length > 50
-                    ? book.title.slice(0, 50) + "..."
-                    : book.title}
-                </h3>
-                <h3 className="text-sm">{book.authors[0]?.name}</h3>
-                <h3 className="text-lg">${bookPrice}</h3>
-                <BookCounter />
-              </div>
-              <img
-                src={book.formats["image/jpeg"]}
-                alt={book.title}
-                className="md:col-start-1 md:row-start-1 md:row-end-3"
-              />
-            </div>
-          );
+          return <CheckoutBooks book={book} bookPrice={bookPrice} key={book.id}/>;
         })}
       </div>
     </div>
---
+++ src/components/CheckoutBooks.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+function CheckoutBooks({ book, bookPrice }) {
+  const [orderCount, setOrderCount] = useState(1);
+
+  function increaseCount() {
+    if (orderCount === 10) return;
+    setOrderCount(orderCount + 1);
+  }
+
+  function decreaseCount() {
+    if (orderCount === 1) return;
+    setOrderCount(orderCount - 1);
+  }
+
+  return (
+    <div
+      key={book.id}
+      className=" w-[75vw] sm:w-[60vw] md:w-[40vw] xl:w-[30vw] bg-gray-50 shadow-xl rounded-xl border-main_yellow border-4 flex flex-col p-6 gap-4 justify-between md:grid md:grid-cols-2"
+    >
+      <div className="flex flex-col gap-1 md:gap-4">
+        <h3 className="text-xl">
+          {book.title.length > 50
+            ? book.title.slice(0, 50) + "..."
+            : book.title}
+        </h3>
+        <h3 className="text-sm">{book.authors[0]?.name}</h3>
+        <h3 className="text-lg">${bookPrice * orderCount}</h3>
+        <div>
+          <button onClick={increaseCount}>+</button>
+          <input type="number" min={0} max={10} value={orderCount} />
+          <button onClick={decreaseCount}>-</button>
+        </div>
+      </div>
+      <img
+        src={book.formats["image/jpeg"]}
+        alt={book.title}
+        className="md:col-start-1 md:row-start-1 md:row-end-3"
+      />
+    </div>
+  );
+}
+
+export default CheckoutBooks;
